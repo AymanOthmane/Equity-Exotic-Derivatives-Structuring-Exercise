954e7dc0e82421945661b8d19a3f6ee44cba5710
Ayman Othmane <148972573+AymanOthmane@users.noreply.github.com>
2024-11-15 17:03:41 +0100

    bat, data and pitch

---
+++ Interface.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Launching Streamlit interface...
+
+python -m streamlit run user_interface.py
+
+pause
---
+++ Librairies.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required Python libraries...
+pip install pandas openpyxl streamlit 
+echo Libraries installed successfully.
+
